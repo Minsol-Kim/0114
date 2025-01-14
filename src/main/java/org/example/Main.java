e1af32537b7e0caa38d96bf8446acf5d7bd79e86
Minsol Kim <yhe071624@gmail.com>
2025-01-14 16:38:13 +0900

    #

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.example.card.Card;
+import org.example.card.CardDeck;
 import org.example.lots.LotsUI;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //카드 2장 서로 나누어가짐
+        Card userCard= CardDeck.INSTANCE.getOne();
+        Card comCard=CardDeck.INSTANCE.getOne();
+
+        //나누어가진 카드 비교
+        System.out.println(userCard);
+        System.out.println(comCard);
+
 //        VendingMachine vm = new VendingMachine(20,5,5);
 //
 //        VendingUI ui =new VendingUI(vm);
@@ -25,8 +35,8 @@ public class Main {
 //
 //        System.out.println(list.remove(0));
 //        System.out.println(list);
-        LotsUI ui = new LotsUI();
-        ui.startGame();
+//        LotsUI ui = new LotsUI();
+//        ui.startGame();
 
 
 
---
+++ src/main/java/org/example/card/CardPattern.java
@@ -0,0 +1,6 @@
+package org.example.card;
+
+public enum CardPattern {
+    DIA,CLO,SPA,HAR; //카드 패턴 -> 다이아, 클로버, 스페이드, 하트
+
+}
